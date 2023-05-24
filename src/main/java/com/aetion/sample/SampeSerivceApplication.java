faf55f0b6e07c943d9591954cc46319a75cfa086
Eric Bronnimann <eric.bronnimann@aetion.com>
2023-05-24 15:54:20 -0400

    Initial commit.

---
+++ src/main/java/com/aetion/sample/SampeSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.aetion.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampeSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampeSerivceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aetion/sample/SampeSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aetion.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampeSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
