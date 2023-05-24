60c33886ec8eb692b63cacc32f6553609ec7214a
Eric Bronnimann <eric.bronnimann@aetion.com>
2023-05-24 17:34:29 -0400

    Adding GitHub action.

--- src/main/java/com/aetion/sample/SampleServiceApplication.java
+++ src/main/java/com/aetion/sample/SampleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampeSerivceApplication {
+public class SampleServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampeSerivceApplication.class, args);
+		SpringApplication.run(SampleServiceApplication.class, args);
 	}
 
 }
